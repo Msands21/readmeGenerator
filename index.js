306b9f580694f4003ef08c5ba3adafcf4907da7d
Mike Sands <mikesands94@gmail.com>
2022-04-21 18:34:16 -0400

    initial boilerplate

---
+++ index.js
@@ -0,0 +1,27 @@
+// TODO: Include packages needed for this application
+const fs = require ('fs');
+const path = require ('path');
+const inquirer = require ('inquirer');
+const generateMarkdown = require ('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// What is your GH username?
+// What is your email address?
+// What is your project title?
+// Please write a short project description
+// What kind of licesnse does your project have?
+// What command should I run to install dependencies?
+// What command should be run for tests?
+// What does your user need to know about using the repository?
+// Who contributed to the repo?
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
