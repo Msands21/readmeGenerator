99c599751cfe01b37801129a5bcacc16532647d4
Mike Sands <mikesands94@gmail.com>
2022-04-21 21:09:42 -0400

    added writetofile and init functions

--- index.js
+++ index.js
@@ -1,27 +1,73 @@
 // TODO: Include packages needed for this application
 const fs = require ('fs');
-const path = require ('path');
 const inquirer = require ('inquirer');
 const generateMarkdown = require ('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ 
+    {
+    type: 'input',
+    message: 'What is your GitHub Username?',
+    name: 'userName'
+    }, {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email'
+    }, {
+    type: 'input',
+    message: 'What is your projects name?',
+    name: 'title'
+    }, {
+    type: 'input',
+    message: 'Please write a short description of your project.',
+    name: 'description'
+    }, {
+    type: 'list',
+    message: 'What kind of license should your project have?',
+    name: 'license',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'NONE'],
+    }, {
+    type: 'input',
+    message: 'What command should be run to install dependencies?',
+    name: 'command'
+    }, {
+    type: 'input',
+    message: 'What does the user need to know about using the repo?',
+    name: 'userRepo'
+    }, {
+    type: 'input',
+    message: 'Who contributed to the repo?',
+    name: 'contributors'
+    }, 
+];
 
-// What is your GH username?
-// What is your email address?
-// What is your project title?
-// Please write a short project description
-// What kind of licesnse does your project have?
-// What command should I run to install dependencies?
-// What command should be run for tests?
-// What does your user need to know about using the repository?
-// Who contributed to the repo?
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./generatedREADME.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve ({
+                ok: true
+            })
+        })
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data) {
+        console.log (data);
+    var fileContent = generateMarkdown(data);
+    writeToFile(fileContent)
+    });
+}
 
 // Function call to initialize app
 init();
+
+module.exports = questions;
