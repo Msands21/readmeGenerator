45f470301e2a97cec558455f746092f6637b6fe2
Mike Sands <mikesands94@gmail.com>
2022-04-21 21:33:06 -0400

    building badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,24 @@
+// Packages needed
+const fs = require('fs');
+const inquirer = require('inquirer');
+const index = require('../index')
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license !== 'None') {
-    return `[License]($(license))`
+  let badge = '';
+  if (license === 'MIT') {
+    badge = `![GitHub license](https://img.shields.io/badge/license-MIT-yellow.svg)`;
+  } else if (license === 'APACHE2.0') {
+    badge = `![GitHub license](https://img.shields.io/badge/license-Apache_2.0-blue.svg)`;
+  } else if (license === 'GPL3.0') {
+    badge = `![GitHub license](https://img.shields.io/badge/license-GPLv3-blue.svg)`;
+  } else if (license === 'BSD 3') {
+    badge = `![GitHub license](https://img.shields.io/badge/license-BSD_3--Clause-blue.svg)`;
+  } else {
+    badge = ''
   }
-  return '';
+  return badge;
 }
 
 // TODO: Create a function that returns the license link
